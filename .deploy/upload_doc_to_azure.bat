9df7a61bb0e3a3452e1b35fefec6cb06f3862a80
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2021-09-09 12:46:25 +0200

    test upload doc

---
+++ .deploy/upload_doc_to_azure.bat
@@ -0,0 +1,1 @@
+az storage blob upload-batch -d output -s ./build/html --destination-path vrs_test_deploy_doc  --connection-string AccountName=swimblobqa;AccountKey=abU4jlu6SA2lUQbQw33pbK8WRpd+YTTujtp67/DgZrQIMaW4osd9bCBNPuW1TkGdsUix+I8uWt4my6j9+wOD3g==;EndpointSuffix=core.windows.net;DefaultEndpointsProtocol=https;
