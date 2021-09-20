8f9547d594f1cc1499854a6b59dfc3bc24a25b3a
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2021-09-20 16:12:55 +0200

    minor changes

--- fourinsight/engineroom/utils/datamanage.py
+++ fourinsight/engineroom/utils/datamanage.py
@@ -5,6 +5,15 @@ import numpy as np
 
 
 class BaseDataSource(ABC):
+
+    @abstractmethod
+    def get_label(self, label, start, end):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def labels(self):
+        raise NotImplementedError()
+
     def _get(self, start, end):
         data = {}
         for label in self.labels():
@@ -19,14 +28,6 @@ class BaseDataSource(ABC):
             if not tolerance:
                 raise ValueError("No tolerance given.")
             return self._synchronize(data, tolerance)
-
-    @abstractmethod
-    def get_label(self, label, start, end):
-        raise NotImplementedError()
-
-    @abstractmethod
-    def labels(self):
-        raise NotImplementedError()
 
     @staticmethod
     def _sync_series(series_a, series_b, tolerance):
@@ -67,7 +68,22 @@ class DrioDataSource(BaseDataSource):
         self._labels = labels
 
     def get_label(self, label, start, end):
+        """
+        Get label data.
+
+        Parameters
+        ----------
+        label : str
+            Label to get data for.
+        start : datetime-like
+            Start time (inclusive) of the series given as anything pandas.to_datetime
+            is able to parse.
+        end : datetime-like
+            Stop time (inclusive) of the series given as anything pandas.to_datetime
+            is able to parse.
+        """
         return self._drio_client.get(self._labels[label], start=start, end=end)
 
     def labels(self):
+        """Data source labels"""
         return tuple(self._labels.keys())
