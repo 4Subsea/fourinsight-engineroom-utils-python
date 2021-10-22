abd61120b200e85b3220427bda9cefb6f9d1cfc9
Ali Cetin <ali.cetin@4subsea.com>
2021-10-22 10:57:20 +0200

    fix: remove obsolete import statement

--- fourinsight/engineroom/utils/core.py
+++ fourinsight/engineroom/utils/core.py
@@ -4,7 +4,6 @@ from collections.abc import MutableMapping
 from io import BytesIO, TextIOWrapper
 from pathlib import Path
 
-import numpy as np
 import pandas as pd
 from azure.core.exceptions import ResourceNotFoundError
 from azure.storage.blob import BlobClient
