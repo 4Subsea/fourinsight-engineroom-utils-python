8b281dae3521ce28a711ebb496410db28fc686e9
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2021-12-02 16:47:05 +0100

    more tests

--- tests/test_datamanage.py
+++ tests/test_datamanage.py
@@ -2,6 +2,7 @@ import types
 from hashlib import md5
 from pathlib import Path
 from unittest.mock import Mock, call, patch
+import os
 
 import numpy as np
 import pandas as pd
@@ -760,6 +761,169 @@ class Test_BaseDataSource:
         df_out = pd.read_feather(cache_dir / "filename")
         df_expect = pd.DataFrame(data={"filename": [2, 4, 6], "a": [1, 2, 3]})
         pd.testing.assert_frame_equal(df_out, df_expect)
+
+    @patch.object(BaseDataSourceForTesting, "_get")
+    def test__cache_source_get(self, mock_get, tmp_path):
+
+        def _get_side_effect(start, end):
+            index = np.arange(start, end+1, dtype="int64")
+            values_a = [1.0] * len(index)
+            values_b = [2.1] * len(index)
+            df = pd.DataFrame(data={"a": values_a, "b": values_b}, index=index)
+            return df
+
+        mock_get.side_effect = _get_side_effect
+
+        class DataSource(BaseDataSourceForTesting):
+            @property
+            def _fingerprint(self):
+                return "1234"
+
+        cache_dir = tmp_path / ".cache"
+        source = DataSource(IntegerIndexConverter(), cache=cache_dir, cache_size=10)
+
+        out_source = source._cache_source_get(5, 105, refresh_cache=False)
+        out_memory_cache = source._cache_source_get(5, 105, refresh_cache=False)
+        source = DataSource(IntegerIndexConverter(), cache=cache_dir, cache_size=10)
+        out_file_cache = source._cache_source_get(5, 105, refresh_cache=False)
+
+        index = np.arange(5, 105+1, dtype="int64")
+        values_a = [1.0] * len(index)
+        values_b = [2.1] * len(index)
+        expect = pd.DataFrame(data={"a": values_a, "b": values_b}, index=index)
+
+        pd.testing.assert_frame_equal(out_source, expect)
+        pd.testing.assert_frame_equal(out_memory_cache, expect)
+        pd.testing.assert_frame_equal(out_file_cache, expect)
+
+        mock_get.assert_has_calls([
+            call(0, 10),
+            call(10, 20),
+            call(20, 30),
+            call(30, 40),
+            call(40, 50),
+            call(50, 60),
+            call(60, 70),
+            call(70, 80),
+            call(80, 90),
+            call(90, 100),
+            call(100, 110),
+        ])
+
+        # memory cache
+        assert len(source._memory_cache) == 11
+        pd.testing.assert_frame_equal(pd.concat(source._memory_cache.values()).sort_index().loc[5:105], expect)
+
+        # file cache
+        df_list = []
+        for fname in os.listdir(cache_dir):
+            df_i = source._cache_read(fname)
+            df_list.append(df_i)
+
+        assert len(os.listdir(cache_dir)) == 11
+        pd.testing.assert_frame_equal(pd.concat(df_list).sort_index().loc[5:105], expect)
+
+    @patch.object(BaseDataSourceForTesting, "_get")
+    def test__cache_source_get_refresh(self, mock_get, tmp_path):
+
+        def _get_side_effect(start, end):
+            index = np.arange(start, end+1, dtype="int64")
+            values_a = [1.0] * len(index)
+            values_b = [2.1] * len(index)
+            df = pd.DataFrame(data={"a": values_a, "b": values_b}, index=index)
+            return df
+
+        mock_get.side_effect = _get_side_effect
+
+        class DataSource(BaseDataSourceForTesting):
+            @property
+            def _fingerprint(self):
+                return "1234"
+
+        cache_dir = tmp_path / ".cache"
+        source = DataSource(IntegerIndexConverter(), cache=cache_dir, cache_size=10)
+
+        source._cache_source_get(5, 105, refresh_cache=True)
+        source._cache_source_get(5, 105, refresh_cache=True)
+
+        assert len(source._memory_cache) == 11
+        assert len(os.listdir(cache_dir)) == 11
+
+        mock_get.assert_has_calls([
+            call(0, 10),
+            call(10, 20),
+            call(20, 30),
+            call(30, 40),
+            call(40, 50),
+            call(50, 60),
+            call(60, 70),
+            call(70, 80),
+            call(80, 90),
+            call(90, 100),
+            call(100, 110),
+            call(0, 10),
+            call(10, 20),
+            call(20, 30),
+            call(30, 40),
+            call(40, 50),
+            call(50, 60),
+            call(60, 70),
+            call(70, 80),
+            call(80, 90),
+            call(90, 100),
+            call(100, 110),
+        ])
+
+    @patch.object(BaseDataSourceForTesting, "_get")
+    def test__cache_source_get_2(self, mock_get, tmp_path):
+
+        def _get_side_effect(start, end):
+            index = np.arange(start, end+1, dtype="int64")
+            values_a = [1.0] * len(index)
+            values_b = [2.1] * len(index)
+            df = pd.DataFrame(data={"a": values_a, "b": values_b}, index=index)
+            return df
+
+        mock_get.side_effect = _get_side_effect
+
+        class DataSource(BaseDataSourceForTesting):
+            @property
+            def _fingerprint(self):
+                return "1234"
+
+        cache_dir = tmp_path / ".cache"
+        source_a = DataSource(IntegerIndexConverter(), cache=cache_dir, cache_size=10)
+        source_b = DataSource(IntegerIndexConverter(), cache=cache_dir, cache_size=10)
+
+        out_a = source_a._cache_source_get(5, 45, refresh_cache=True)
+        out_b = source_b._cache_source_get(5, 105, refresh_cache=True)
+
+        mock_get.assert_has_calls([
+            call(0, 10),
+            call(10, 20),
+            call(20, 30),
+            call(30, 40),
+            call(40, 50),
+            call(50, 60),
+            call(60, 70),
+            call(70, 80),
+            call(80, 90),
+            call(90, 100),
+            call(100, 110),
+        ])
+
+        index = np.arange(5, 45+1, dtype="int64")
+        values_a = [1.0] * len(index)
+        values_b = [2.1] * len(index)
+        expect_a = pd.DataFrame(data={"a": values_a, "b": values_b}, index=index)
+
+        index = np.arange(5, 105+1, dtype="int64")
+        values_a = [1.0] * len(index)
+        values_b = [2.1] * len(index)
+        expect_b = pd.DataFrame(data={"a": values_a, "b": values_b}, index=index)
+
+        pd.testing.assert_frame_equal(out_a, expect_a)
+        pd.testing.assert_frame_equal(out_b, expect_b)
 
 
 class Test_DrioDataSource:
