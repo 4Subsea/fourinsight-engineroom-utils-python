b7488823b8f565e254a6f83bf20a75ff3f8e93e5
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2025-05-12 11:50:03 +0200

    Black .

--- tests/test_core.py
+++ tests/test_core.py
@@ -953,11 +953,9 @@ class Test_ResultCollector:
         results.truncate(before="2020-01-01 01:00", after="2020-01-01 03:00")
         df_out = results.dataframe
         df_expect = pd.DataFrame(
-            data={
-            },
+            data={},
             index=pd.DatetimeIndex(
-                [
-                ],
+                [],
                 tz="utc",
             ),
         ).astype({"a": "float64", "b": "string", "c": "Int64", "d": "float64"})
